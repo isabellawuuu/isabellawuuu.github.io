6ee7a12d66decb6b81f68fb16eff62674525200e
isabellawuuu <isabellawu321@gmail.com>
2026-03-04 19:03:59 -0500

    Update global.js

--- data/global.js
+++ data/global.js
@@ -14,7 +14,7 @@ const globalData = {
   phone: "",
   email: "egg4pb@virginia.edu",
   linkedinTitle: "LinkedIn",
-  linkedin: "https://www.linkedin.com/in/shu-wu-404aa4239",
+  linkedin: "https://www.linkedin.com/in/shu-wu-404aa4239/",
   //orcidTitle: "Orcid Username",
   //orcid: "https://orcid.com/your_username",
 };
